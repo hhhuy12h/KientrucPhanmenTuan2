39948c4d61d27c4b1ffa329a33d58239e9066c8e
hhhuy12h <Sothunhat28@gmail.com>
2021-09-15 02:31:22 -0700

    commit

---
+++ src/main/java/com/example/huy2h/Huy2hApplication.java
@@ -0,0 +1,13 @@
+package com.example.huy2h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Huy2hApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Huy2hApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/huy2h/Huy2hApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.huy2h;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Huy2hApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
