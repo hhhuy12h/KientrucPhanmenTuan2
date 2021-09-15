86c5b3ebc6dbc50a2b8dfa9d6f263b898cfde09b
hhhuy12h <Sothunhat28@gmail.com>
2021-09-15 02:42:29 -0700

    b

---
+++ src/main/java/com/example/huy2h/Entity/Student.java
@@ -0,0 +1,29 @@
+package com.example.huy2h.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "email")
+    private String email;
+}
